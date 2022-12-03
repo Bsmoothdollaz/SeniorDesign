3cf7ad374b87174a6f934cac3a8465eed9654557
ckaniero <95322855+chikeanierobi@users.noreply.github.com>
2022-12-03 14:08:29 -0500

    Commit Dec 3rd
    Developed a UI

--- main.py
+++ main.py
@@ -2,60 +2,77 @@ import sys
 import time
 import cv2
 import logging
-import constants
+import constants as telloConstants
 from DJITelloPy.api import Tello
 from CameraController import CameraController
 import threading
+import tkinter
+from tkinter import *
+from DJITelloPy.api import GUI
+buttonPressed = False
+
+
+
+
+
+
+
+
+
 
 state_logging_interval = 0.5  # seconds
 mission_pad_logging_interval = 0.5
 
 
-def mission_pad_logger(tello, mission_pad_logging_interval):
+
+def mission_pad_logger(telloDrone, mission_pad_logging_interval):
     time.sleep(mission_pad_logging_interval)
-    if tello.get_mission_pad_id() != 1:
-        tello.LOGGER.info('misison pad number detected:{}'.format(tello.get_mission_pad_id()))
+    if telloDrone.get_mission_pad_id() != 1:
+        telloDrone.LOGGER.info('misison pad number detected:{}'.format(tello.get_mission_pad_id()))
 
-        move_x = tello.get_mission_pad_distance_x()
-        move_y = tello.get_mission_pad_distance_y()
-        move_z = tello.get_mission_pad_distance_z()
+        move_x = telloDrone.get_mission_pad_distance_x()
+        move_y = telloDrone.get_mission_pad_distance_y()
+        move_z = telloDrone.get_mission_pad_distance_z()
 
-    mission_pad_logger(tello, mission_pad_logging_interval)
-
-
-def log_before_execution(tello):
-    tello.LOGGER.info('data goes here')
-    tello.LOGGER.info('ATTITUDE: {}'.format(tello.query_attitude()))
-    tello.LOGGER.info('BAROMETER: {}'.format(tello.query_barometer()))
-    tello.LOGGER.info('BATTERY: {}'.format(tello.query_battery()))
-    tello.LOGGER.info('TOF_DISTANCE: {} '.format(tello.query_distance_tof()))
-    tello.LOGGER.info('WIFI_SIGNAL_NOISE RATIO: {}'.format(tello.query_wifi_signal_noise_ratio()))
-    tello.LOGGER.info('SDK VERSION: {}'.format(tello.query_sdk_version()))
-    tello.LOGGER.info('SERIAL NUMBER: {}'.format(tello.query_serial_number()))
+    mission_pad_logger(telloDrone, mission_pad_logging_interval)
 
 
-def log_state(interval_sec, tello):
+def log_before_execution(telloDrone):
+    telloDrone.LOGGER.info('data goes here')
+    telloDrone.LOGGER.info('ATTITUDE: {}'.format(telloDrone.query_attitude()))
+    telloDrone.LOGGER.info('BAROMETER: {}'.format(telloDrone.query_barometer()))
+    telloDrone.LOGGER.info('BATTERY: {}'.format(telloDrone.query_battery()))
+    telloDrone.LOGGER.info('TOF_DISTANCE: {} '.format(telloDrone.query_distance_tof()))
+    telloDrone.LOGGER.info('WIFI_SIGNAL_NOISE RATIO: {}'.format(telloDrone.query_wifi_signal_noise_ratio()))
+    telloDrone.LOGGER.info('SDK VERSION: {}'.format(telloDrone.query_sdk_version()))
+    telloDrone.LOGGER.info('SERIAL NUMBER: {}'.format(telloDrone.query_serial_number()))
+
+
+def log_state(interval_sec, telloDrone):
     time.sleep(interval_sec)
-    tello.LOGGER.info('PERIODIC STATE VALUES: {}'.format(tello.get_current_state()))
-    log_state(interval_sec, tello)
+    telloDrone.LOGGER.info('PERIODIC STATE VALUES: {}'.format(telloDrone.get_current_state()))
+    log_state(interval_sec, telloDrone)
 
 
 
 
 """ CONNECT TO THE DRONE"""
-tello = Tello()
-tello.LOGGER.info(constants.MESSAGES.try_connect_drone)
+telloDrone = Tello()
+telloDrone.LOGGER.info(telloConstants.MESSAGES.try_connect_drone)
 
 try:
-    tello.connect()
+    telloDrone.connect()
 except Exception as e:
-    tello.LOGGER.error(constants.MESSAGES.failed_connect_drone)
+    telloDrone.LOGGER.error(telloConstants.MESSAGES.failed_connect_drone)
     sys.exit('*** Exiting program. Could not connect to the drone.***')
 
-tello.LOGGER.info(constants.MESSAGES.successful_connect_drone)
+telloDrone.LOGGER.info(telloConstants.MESSAGES.successful_connect_drone)
+
+# Function to move up
+
 
 # Start the periodic drone state logger
-state_logger = threading.Thread(target=log_state, args=(state_logging_interval, tello), daemon=True, name='state-logger')
+state_logger = threading.Thread(target=log_state, args=(state_logging_interval, telloDrone), daemon=True, name='state-logger')
 state_logger.start()
 
 # tello.set_video_bitrate(Tello.BITRATE_1MBPS)
@@ -64,12 +81,12 @@ camera = CameraController(tello=tello)
 # camera.run_bottom_cam()
 camera.run_front_cam()
 """ DO SOME PRE-FLIGHT ACTIONS """
-tello.enable_mission_pads()
-tello.set_mission_pad_detection_direction(0)
+telloDrone.enable_mission_pads()
+telloDrone.set_mission_pad_detection_direction(0)
 start_time = time.time()  # start the flight timer
-tello.turn_motor_on()
+telloDrone.turn_motor_on()
 try:
-    log_before_execution(tello)
+    log_before_execution(telloDrone)
 except Exception as e:
     pass
 
@@ -84,20 +101,7 @@ tello.takeoff()
 
 """ USER INPUT TO MOVE DRONE IN DIFFERENT DIRECTIONS"""
 
-# moveUp = float(input("Enter distance to move up: "))
-# moveDown = float(input("Enter distance to move down: "))
-# moveLeft = float(input("Enter distance to move left: "))
-# moveRight = float(input("Enter distance to move right: "))
 
-
-# tello.move("up", 400)
-# time.sleep(3)
-# tello.move_down(400)
-# time.sleep(3)
-# tello.move_left(400)
-# time.sleep(3)
-# tello.move_right(400)
-# time.sleep(3)
 
 
 """ READY TO LAND THE DRONE"""
@@ -106,9 +110,15 @@ tello.turn_motor_on()
 time.sleep(5)
 tello.turn_motor_off()
 
+
+
+
 """ Gracefully close any resources """
 # daemon threads are joined by default
 # log_before_execution(tello)
 tello.streamoff()
 tello.LOGGER.info("EXECUTION TIME: --- %s seconds ---" % (time.time() - start_time))
 exit(1)
+
+
+
