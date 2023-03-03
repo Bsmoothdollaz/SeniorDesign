ac037b12a18985c7e5c46e1b6466bc84ea5a8648
Alex Perez <acperez@syr.edu>
2023-03-03 16:50:47 -0500

    update endpoints and drone handles for more functionality

--- frontend2/app/app.py
+++ frontend2/app/app.py
@@ -26,6 +26,7 @@ global drone_wrapper
 drone_wrapper = test_drone_connection.Custom_Drone()
 drone_wrapper.try_connect()
 
+
 @app.route("/")
 def view_home():
     return render_template("index.html", title="Home")
@@ -51,7 +52,7 @@ def view_control():
     return render_template("control.html", title="Control")
 
 
-@app.route("/validate_flight_plan", methods=["POST"]) 
+@app.route("/validate_flight_plan", methods=["POST"])
 def validate_flight_plan():
     f_plan = request.form['fplanfield']
     # At this point we have f_plan as a python variable that we can parse
@@ -82,12 +83,117 @@ def get_connection_status():
 @app.route("/takeoff", methods=["POST"])
 def tello_takeoff():
     global drone_wrapper
-    drone_wrapper.takeoff()
+    takeoff_thread = Thread(target=drone_wrapper.takeoff, args=())
+    takeoff_thread.start()
     return "True"
 
 
 @app.route("/land", methods=["POST"])
 def tello_land():
     global drone_wrapper
-    drone_wrapper.land()
+    land_thread = Thread(target=drone_wrapper.land, args=())
+    land_thread.start()
+    # drone_wrapper.land()
     return "True"
+
+
+@app.route("/connect_to_drone", methods=["POST"])
+def connect_to_drone():
+    global drone_wrapper
+    drone_wrapper.try_connect()
+    return "True"
+
+
+@app.route("/emergency_shutoff", methods=["POST"])
+def emergency_kill_drone():
+    global drone_wrapper
+    emergency_thread = Thread(target=drone_wrapper.emergency, args=())
+    emergency_thread.start()
+
+
+@app.route("/motor_on", methods=["POST"])
+def turn_motor_on():
+    global drone_wrapper
+    motor_on_thread = Thread(target=drone_wrapper.motor_on, args=())
+    motor_on_thread.start()
+
+
+@app.route("/motor_off", methods=["POST"])
+def turn_motor_off():
+    global drone_wrapper
+    motor_off_thread = Thread(target=drone_wrapper.motor_off, args=())
+    motor_off_thread.start()
+
+
+@app.route("/flip_forward", methods=["POST"])
+def flip_forward():
+    global drone_wrapper
+    flip_forward_thread = Thread(target=drone_wrapper.flip, args=('f',))
+    flip_forward_thread.start()
+
+
+@app.route("/flip_backwards", methods=["POST"])
+def flip_back():
+    global drone_wrapper
+    flip_back_thread = Thread(target=drone_wrapper.flip, args=('b',))
+    flip_back_thread.start()
+
+
+@app.route("/flip_left", methods=["POST"])
+def flip_left():
+    global drone_wrapper
+    flip_left_thread = Thread(target=drone_wrapper.flip_forward, args=('l',))
+    flip_left_thread.start()
+
+
+@app.route("/flip_right", methods=["POST"])
+def flip_right():
+    global drone_wrapper
+    flip_right_thread = Thread(target=drone_wrapper.flip_forward, args=('r',))
+    flip_right_thread.start()
+
+
+@app.route("/set_speed", methods=["POST"])
+def set_drone_speed():
+    global drone_wrapper
+    speed = int(request.form["speed"])
+    if speed < 10 or speed > 100:
+        print('cant set speed to that amount')
+        return 'Bad speed amount'
+    set_speed_thread = Thread(target=drone_wrapper.set_speed, args=(speed,))
+    set_speed_thread.start()
+
+
+@app.route("/drone_move_udlr", methods=["POST"])
+def drone_move_udlr():
+    global drone_wrapper
+    direction = str(request.form["direction"])
+    distance = int(request.form["distance"])
+    directions = ['up', 'down', 'left', 'right', 'forward', 'back']
+    if direction not in directions:
+        print('bad direction value')
+        return 'Bad direction value'
+    if distance < 20 or distance > 500:
+        print('cant set distance to that amount')
+        return 'Bad distance value'
+    drone_udlr_thread = Thread(target=drone_wrapper.go_up_down_left_right, args=(direction,distance,))
+    drone_udlr_thread.start()
+    return 'here'
+
+
+@app.route("/drone_rotate", methods=["POST"])
+def rotate_drone():
+    global drone_wrapper
+    direction = str(request.form["direction"])
+    degrees = int(request.form["degrees"])
+    directions = ['cw', 'ccw']
+    if direction not in directions:
+        print('bad direction value')
+        return 'bad direction value'
+    if degrees < 1 or degrees > 360:
+        print('cant rotate  to that amount')
+        return 'bad degrees value'
+    drone_rotate_thread = Thread(target=drone_wrapper.rotate_drone, args=(direction,degrees))
+    drone_rotate_thread.start()
+    return 'here'
+
--- test_drone_connection.py
+++ test_drone_connection.py
@@ -19,6 +19,7 @@ class Custom_Drone:
             self.drone.connect()
         except Exception as e:
             print('could not connect to drone')
+            self.drone = None
 
     def get_battery(self):
         drone = self.get_drone_object()
@@ -45,3 +46,51 @@ class Custom_Drone:
         except Exception as e:
             print('exception on land', e)
 
+    def emergency(self):
+        drone = self.get_drone_object()
+        try:
+            drone.send_command_without_return("emergency")
+        except Exception as E:
+            print('exception on emergency', e)
+
+    def motor_on(self):
+        drone = self.get_drone_object()
+        try:
+            drone.send_control_command("motoron")
+        except Exception as E:
+            print('exception on motor on', e)
+
+    def motor_off(self):
+        drone = self.get_drone_object()
+        try:
+            drone.send_control_command("motoroff")
+        except Exception as E:
+            print('exception on motor off', e)
+
+    def flip(self, direction):
+        drone = self.get_drone_object()
+        try:
+            drone.send_control_command("flip {}".format(direction))
+        except Exception as e:
+            print('exception on flip', e)
+
+    def set_speed(self, speed):
+        drone = self.get_drone_object()
+        try:
+            drone.send_control_command("speed {}".format(speed))
+        except Exception as e:
+            print('exception on set speed', e)
+
+    def go_up_down_left_right(self, direction, distance):
+        drone = self.get_drone_object()
+        try:
+            drone.send_control_command("{} {}".format(direction, distance))
+        except Exception as e:
+            print('exception on up down left right', e)
+
+    def rotate_drone(self, direction, degrees):
+        drone = self.get_drone_object()
+        try:
+            drone.send_control_command("{} {}".format(direction, degrees))
+        except Exception as e:
+            print('exception rotate drone', e)
